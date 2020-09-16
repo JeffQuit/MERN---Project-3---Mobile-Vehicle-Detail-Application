f3d696e344c4ce953472db98789859cfe568685b
Jonathan <jonathan213kim@gmail.com>
2020-09-15 20:13:18 -0700

    updated socialmedia page

--- client/src/components/Socialmedia/Socialmedia.js
+++ client/src/components/Socialmedia/Socialmedia.js
@@ -1,102 +1,120 @@
 import React from "react";
-import { MDBMask, MDBView, MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import {
+  MDBMask,
+  MDBView,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCardTitle,
+  MDBCard,
+  MDBCardImage,
+  MDBCardBody,
+  MDBCardText,
+} from "mdbreact";
 import "./Socialmedia.css";
 
 export default function Socialmedia() {
   return (
-    // <div className="Socialmedia-Full-Container">
+    <div className="Socialmedia-Full-Container">
+      <MDBContainer className="mt-5">
+        <MDBCardTitle className="h2">Instagram</MDBCardTitle>
+        <MDBRow>
+          {/* <MDBCol md="4">
+            <MDBView hover>
+              <img src="/Assets/Images/img3.jpg" className="img-fluid" alt="" />
+              <a
+                href="https://www.instagram.com/p/BZOwFWXHF5m/"
+                target="_blank"
+              >
+                <MDBMask className="flex-center" overlay="red-slight">
+                  <p className="white-text">Detailing done for @ends_uno</p>
+                </MDBMask>
+              </a>
+            </MDBView>
+          </MDBCol> */}
+          <MDBCol>
+            <MDBCard className="mx-auto" style={{ width: "20rem" }}>
+              <a
+                href="https://www.instagram.com/p/BZOwFWXHF5m/"
+                target="_blank"
+              >
+                <MDBCardImage
+                  className="img-fluid"
+                  src="/Assets/Images/img3.jpg"
+                  waves
+                />
+              </a>
+              <MDBCardBody>
+                {/* <MDBCardTitle>Title</MDBCardTitle> */}
+                <MDBCardText>Detailing done for @ends_uno</MDBCardText>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+          {/* <MDBCol md="4">
+            <MDBView hover>
+              <img src="/Assets/Images/img2.jpg" className="img-fluid" alt="" />
+              <a
+                href="https://www.instagram.com/p/BY6aa9CHW5F/"
+                target="_blank"
+              >
+                <MDBMask className="flex-center" overlay="red-slight">
+                  <p className="white-text">Detailing done for @ends_uno</p>
+                </MDBMask>
+              </a>
+            </MDBView>
+          </MDBCol> */}
+          <MDBCol>
+            <MDBCard className="mx-auto" style={{ width: "20rem" }}>
+              <a
+                href="https://www.instagram.com/p/BY6aa9CHW5F/"
+                target="_blank"
+              >
+                <MDBCardImage
+                  className="img-fluid"
+                  src="/Assets/Images/img2.jpg"
+                  waves
+                />
+              </a>
+              <MDBCardBody>
+                {/* <MDBCardTitle>Title</MDBCardTitle> */}
+                <MDBCardText>Detailing done for @ends_uno</MDBCardText>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+          {/* <MDBCol md="4">
+            <MDBView hover>
+              <img src="/Assets/Images/img1.jpg" className="img-fluid" alt="" />
+              <a
+                href="https://www.instagram.com/p/B0KSDpgH0Ph/"
+                target="_blank"
+              >
+                <MDBMask className="flex-center" overlay="red-slight">
+                  <p className="white-text">Detailing done for @1nice_subie</p>
+                </MDBMask>
+              </a>
+            </MDBView>
+          </MDBCol> */}
 
-    <MDBContainer className="mt-5">
-      <MDBRow>
-        <MDBCol md="4">
-          <MDBView hover>
-            <img
-              src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
-              className="img-fluid"
-              alt=""
-            />
-            {/* 
-				To use when mapping images
-				`url(${img.url})` 
-
-				Template literal to grab the Image's URL, as well as the IG Page URL.
-			*/}
-            <a href="https://www.google.com" target="_blank">
-              <MDBMask className="flex-center" overlay="red-slight">
-                <p className="white-text">Example Text of image description</p>
-              </MDBMask>
-            </a>
-          </MDBView>
-        </MDBCol>
-
-        <MDBCol md="4">
-          <MDBView hover>
-            <img
-              src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
-              className="img-fluid"
-              alt=""
-            />
-
-            <MDBMask className="flex-center" overlay="red-slight">
-              <p className="white-text">More Example Text</p>
-            </MDBMask>
-          </MDBView>
-        </MDBCol>
-        <MDBCol md="4">
-          <MDBView hover>
-            <img
-              src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
-              className="img-fluid"
-              alt=""
-            />
-            <MDBMask className="flex-center" overlay="red-slight">
-              <p className="white-text">
-                Hey, look at that, more example text for instagram
-              </p>
-            </MDBMask>
-          </MDBView>
-        </MDBCol>
-      </MDBRow>
-      <MDBRow className="mt-4">
-        <MDBCol md="4">
-          <MDBView hover zoom>
-            <img
-              src="https://mdbootstrap.com/img/Others/documentation/img%20(131)-mini.jpg"
-              className="img-fluid"
-              alt=""
-            />
-            <MDBMask className="flex-center">
-              <p className="white-text">Zoom effect</p>
-            </MDBMask>
-          </MDBView>
-        </MDBCol>
-
-        <MDBCol md="4">
-          <MDBView hover>
-            <img
-              src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
-              className="img-fluid"
-              alt=""
-            />
-            <MDBMask className="flex-center" overlay="red-slight">
-              <p className="white-text">Example Text of image description</p>
-            </MDBMask>
-          </MDBView>
-        </MDBCol>
-        <MDBCol md="4">
-          <MDBView hover>
-            <img
-              src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
-              className="img-fluid"
-              alt=""
-            />
-            <MDBMask className="flex-center" overlay="red-slight">
-              <p className="white-text">Example Text of image description</p>
-            </MDBMask>
-          </MDBView>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-    // </div>
+          <MDBCol>
+            <MDBCard className="mx-auto" style={{ width: "20rem" }}>
+              <a
+                href="https://www.instagram.com/p/B0KSDpgH0Ph/"
+                target="_blank"
+              >
+                <MDBCardImage
+                  className="img-fluid"
+                  src="/Assets/Images/img1.jpg"
+                  waves
+                />
+              </a>
+              <MDBCardBody>
+                {/* <MDBCardTitle>Title</MDBCardTitle> */}
+                <MDBCardText>Detailing done for @1nice_subie</MDBCardText>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </div>
   );
 }
