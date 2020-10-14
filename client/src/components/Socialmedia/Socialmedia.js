3e3e0aab74f1adb70a53ac7f3110f5e45cd91750
Jeff Q <Jeffquittman@gmail.com>
2020-10-14 08:03:53 -0700

    updated titles to gallery and links. Added mobile responsive css to gallery page

--- client/src/components/Socialmedia/Socialmedia.js
+++ client/src/components/Socialmedia/Socialmedia.js
@@ -1,63 +1,45 @@
-import React from "react";
+import React from 'react';
 import {
-  MDBContainer,
-  // MDBRow,
-  // MDBCol,
-  // MDBCardTitle,
-  // MDBCard,
-  // MDBCardImage,
-  // MDBCardBody,
-  // MDBCardText,
-  MDBCarousel,
-  MDBCarouselInner,
-  MDBCarouselItem,
-  MDBView,
-} from "mdbreact";
-import "./Socialmedia.css";
+	MDBContainer,
+	// MDBRow,
+	// MDBCol,
+	// MDBCardTitle,
+	// MDBCard,
+	// MDBCardImage,
+	// MDBCardBody,
+	// MDBCardText,
+	MDBCarousel,
+	MDBCarouselInner,
+	MDBCarouselItem,
+	MDBView,
+} from 'mdbreact';
+import './Socialmedia.css';
 
 export default function Socialmedia() {
-  return (
-    <div className="Socialmedia-Full-Container">
-      <MDBContainer>
-        <MDBCarousel
-          activeItem={1}
-          length={3}
-          showControls={true}
-          showIndicators={false}
-          className="z-depth-1"
-          slide
-        >
-          <MDBCarouselInner>
-            <MDBCarouselItem itemId="1">
-              <MDBView>
-                <img
-                  className="d-block w-100"
-                  src="/Assets/Images/socialmedia1.jpg"
-                  alt="First slide"
-                />
-              </MDBView>
-            </MDBCarouselItem>
-            <MDBCarouselItem itemId="2">
-              <MDBView>
-                <img
-                  className="d-block w-100"
-                  src="/Assets/Images/socialmedia2.jpg"
-                  alt="Second slide"
-                />
-              </MDBView>
-            </MDBCarouselItem>
-            <MDBCarouselItem itemId="3">
-              <MDBView>
-                <img
-                  className="d-block w-100"
-                  src="/Assets/Images/socialmedia3.jpg"
-                  alt="Third slide"
-                />
-              </MDBView>
-            </MDBCarouselItem>
-          </MDBCarouselInner>
-        </MDBCarousel>
-      </MDBContainer>
-    </div>
-  );
+	return (
+		<div className="Socialmedia-Full-Container" id="gallery">
+			<h2 className="Socialmedia-Section-Title">Image Gallery</h2>
+			<MDBContainer>
+				<MDBCarousel activeItem={1} length={3} showControls={true} showIndicators={false} className="z-depth-1" slide>
+					<MDBCarouselInner>
+						<MDBCarouselItem itemId="1">
+							<MDBView>
+								<img className="d-block w-100" src="/Assets/Images/socialmedia1.jpg" alt="First slide" />
+							</MDBView>
+						</MDBCarouselItem>
+						<MDBCarouselItem itemId="2">
+							<MDBView>
+								<img className="d-block w-100" src="/Assets/Images/socialmedia2.jpg" alt="Second slide" />
+							</MDBView>
+						</MDBCarouselItem>
+						<MDBCarouselItem itemId="3">
+							<MDBView>
+								<img className="d-block w-100" src="/Assets/Images/socialmedia3.jpg" alt="Third slide" />
+							</MDBView>
+						</MDBCarouselItem>
+					</MDBCarouselInner>
+				</MDBCarousel>
+			</MDBContainer>
+		</div>
+	);
 }
