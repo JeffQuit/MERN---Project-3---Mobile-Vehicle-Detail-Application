9b0d362d9545e86c0b4ecdf384e9cb06beff59a8
Jeff Q <Jeffquittman@gmail.com>
2020-10-22 18:26:51 -0700

    updating mail controller

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -1,85 +1,71 @@
-const sgMail = require("@sendgrid/mail"); //sendgrid library to send emails
+const sgMail = require('@sendgrid/mail'); //sendgrid library to send emails
 
 exports.sendContactMail = function (req, res) {
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+	sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  //Get Variables from query string in the search bar
-  const { sender, topic, text, name, phone, vehicle } = req.query;
-  //Sendgrid Data Requirements
-  const msg = {
-    to: sender, //recipient
-    from: "gerayanez@gmail.com",
-    subject: topic,
-    text: `
+	//Get Variables from query string in the search bar
+	const { sender, topic, text, name, phone, vehicle } = req.query;
+	//Sendgrid Data Requirements
+	const msg = {
+		to: sender, //recipient
+		from: 'gerayanez@gmail.com',
+		subject: topic,
+		text: `
 	From:\n
 	Name: ${name}\n
 	Email: ${sender}\n
 	Phone: ${phone}\n
 	Vehicle: ${vehicle}\n
 	Main Message: ${text}.`,
-  };
-  //Send Email
-  sgMail.send(msg).then(
-    () => {},
-    (error) => {
-      console.error(error);
+	};
+	//Send Email
+	sgMail.send(msg).then(
+		() => {},
+		(error) => {
+			console.error(error);
 
-      if (error.response) {
-        console.error(error.response.body);
-      }
-    }
-  );
+			if (error.response) {
+				console.error(error.response.body);
+			}
+		}
+	);
 };
 exports.sendClientMail = function (req, res) {
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+	sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  //Get Variables from query string in the search bar
-  const { email, name } = req.query;
-  //Sendgrid Data Requirements
-  const msg = {
-    to: email,
-    from: "gerayanez@gmail.com",
-    subject: "Thank you for booking!",
-    text: `
+	//Get Variables from query string in the search bar
+	const { email, name } = req.query;
+	//Sendgrid Data Requirements
+	const msg = {
+		to: email,
+		from: 'gerayanez@gmail.com',
+		subject: 'Thank you for booking!',
+		text: `
 	Thanks ${name}! We'll reach back out to you!`,
-  };
-  //Send Email
-  sgMail.send(msg).then(
-    () => {},
-    (error) => {
-      console.error(error);
+	};
+	//Send Email
+	sgMail.send(msg).then(
+		() => {},
+		(error) => {
+			console.error(error);
 
-      if (error.response) {
-        console.error(error.response.body);
-      }
-    }
-  );
+			if (error.response) {
+				console.error(error.response.body);
+			}
+		}
+	);
 };
 exports.sendBookedMail = function (req, res) {
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+	sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  //Get Variables from query string in the search bar
-  const {
-    phone,
-    email,
-    address1,
-    name,
-    address2,
-    city,
-    state,
-    zip,
-    makemodel,
-    notes,
-    carlocation,
-    datereq,
-    timereq,
-  } = req.query;
-  //Sendgrid Data Requirements
-  const msg = {
-    to: "Gerasautodetailing@gmail.com",
-    from: "gerayanez@gmail.com",
-    subject: name + " has booked with you.",
-    text: `
+	//Get Variables from query string in the search bar
+	const { phone, email, address1, name, address2, city, state, zip, makemodel, notes, carlocation, datereq, timereq } = req.query;
+	//Sendgrid Data Requirements
+	const msg = {
+		to: 'Gerasautodetailing@gmail.com',
+		from: 'gerayanez@gmail.com',
+		subject: name + ' has booked with you.',
+		text: `
 	From:\n
 	Name: ${name}\n
 	Email: ${email}\n
@@ -93,16 +79,16 @@ exports.sendBookedMail = function (req, res) {
     Date: ${datereq}\n
     Time: ${timereq}\n
 	Notes: ${notes}.`,
-  };
-  //Send Email
-  sgMail.send(msg).then(
-    () => {},
-    (error) => {
-      console.error(error);
+	};
+	//Send Email
+	sgMail.send(msg).then(
+		() => {},
+		(error) => {
+			console.error(error);
 
-      if (error.response) {
-        console.error(error.response.body);
-      }
-    }
-  );
+			if (error.response) {
+				console.error(error.response.body);
+			}
+		}
+	);
 };
